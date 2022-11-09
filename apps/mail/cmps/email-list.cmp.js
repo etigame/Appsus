9a99d98aa9a0efa75a044d38065c8dfc3a0add1f
Eti Gal-Or <etim139@gmail.com>
2022-11-09 21:26:00 +0200

    email-preview

--- apps/mail/cmps/email-list.cmp.js
+++ apps/mail/cmps/email-list.cmp.js
@@ -1,7 +1,27 @@
+import emailPreview from './email-preview.cmp.js'
+
 export default {
+    props:['emails'],
     template: `
-    <section className="email-list">
-        <h1>email list</h1>
-    </section>
-    `
+        <section class="email-list">
+            <ul class="clean-list">
+                <li v-for="email in emails" :key="email.id">
+                    <email-preview :email="email"/>
+                    <!-- <section class="actions">
+                        <router-link :to="'/car/' + car.id">Details</router-link> |
+                        <router-link :to="'/car/edit/' + car.id">Edit</router-link> |
+                        <button @click="remove(car.id)">x</button>
+                    </section> -->
+                </li>
+            </ul>
+        </section>
+    `,
+    methods: {
+        // remove(carId){
+        //     this.$emit('remove', carId)
+        // },
+    },
+    components: {
+        emailPreview,
+    }
 }
--- apps/mail/cmps/email-preview.cmp.js
+++ apps/mail/cmps/email-preview.cmp.js
@@ -1,7 +1,25 @@
 export default {
-    template: `
-    <section className="email-preview">
-        <h1>email preview</h1>
-    </section>
-    `
+  props: ['email'],
+  template: `
+        <article class="email-preview flex" :class="fontStyle">
+            <button className="btn-star">star</button>
+            <button className="btn-important">important</button>
+            <p className="from">{{ email.from }}</p>
+            <p className="subject">{{ email.subject}}</p>
+            <p className="sent-time">{{ displaySentTime }}</p>
+        </article>
+    `,
+  computed: {
+    displaySentTime() {
+      const sentTime = new Date(this.email.sentAt).toLocaleTimeString([], {
+        hour: 'numeric',
+        minute: '2-digit',
+      })
+      return sentTime
+    },
+    fontStyle() {
+      // return this.email.isRead ? 'read' : 'unread'
+      return { read: this.email.isRead, unread: !this.email.isRead }
+    },
+  },
 }
