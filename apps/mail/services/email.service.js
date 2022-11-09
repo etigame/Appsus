12c9d2e5863d8a703531e6b7484722efc508af16
Eti Gal-Or <etim139@gmail.com>
2022-11-09 15:46:06 +0200

    add demo data

--- apps/mail/services/email.service.js
+++ apps/mail/services/email.service.js
@@ -1,67 +1,73 @@
 import { utilService } from '../../../services/util.service.js'
 import { storageService } from '../../../services/async-storage.service.js'
+import gEmails from '../data/emails.json' assert {type: 'json'}
 
 const EMAIL_KEY = 'emailDB'
+
+const loggedinUser = {
+  email: 'etironi100@appsus.com',
+  fullname: 'Eti Gal-Or Roni Siles',
+}
+
 _createEmails()
 
 export const emailService = {
-    query,
-    get,
-    remove,
-    save,
-    getEmptyEmail,
-    getNextEmailId
+  query,
+  get,
+  remove,
+  save,
+  getEmptyEmail,
+  getNextEmailId,
 }
 
 function query() {
-    return storageService.query(EMAIL_KEY)
+  return storageService.query(EMAIL_KEY)
 }
 
-function get(emailId){
-    return storageService.get(EMAIL_KEY, emailId)
+function get(emailId) {
+  return storageService.get(EMAIL_KEY, emailId)
 }
 
 function remove(emailId) {
-    return storageService.remove(EMAIL_KEY, emailId)
+  return storageService.remove(EMAIL_KEY, emailId)
 }
 
 function save(email) {
-    if(email.id){
-        return storageService.put(EMAIL_KEY, email)
-    } else {
-        return storageService.post(EMAIL_KEY, email)
-    }
+  if (email.id) {
+    return storageService.put(EMAIL_KEY, email)
+  } else {
+    return storageService.post(EMAIL_KEY, email)
+  }
 }
 
 function getEmptyEmail() {
-    // return { id: '', vendor, maxSpeed}
+  // return { id: '', vendor, maxSpeed}
 }
 
 function getNextEmailId(emailId) {
-    return storageService.query(EMAIL_KEY)
-        .then(emails =>{
-            var idx  = emails.findIndex(email => email.id === emailId)
-            if (idx === emails.length-1) idx = -1
-            return emails[idx+1].id
-        })
+  return storageService.query(EMAIL_KEY).then((emails) => {
+    var idx = emails.findIndex((email) => email.id === emailId)
+    if (idx === emails.length - 1) idx = -1
+    return emails[idx + 1].id
+  })
 }
 
 function _createEmails() {
-    let emails = utilService.loadFromStorage(EMAIL_KEY)
-    if (!emails || !emails.length) {
-        emails = []
-        // emails.push(_createEmail('Audu Mea', 300))
-        // cars.push(_createCar('Fiak Ibasa', 120))
-        // cars.push(_createCar('Subali Pesha', 100))
-        // cars.push(_createCar('Mitsu Bashi', 150))
-        
-        utilService.saveToStorage(EMAIL_KEY, emails)
-    }
-    return emails
+  let emails = utilService.loadFromStorage(EMAIL_KEY)
+  if (!emails || !emails.length) {
+    emails = gEmails
+    // emails.push(_createEmail('Audu Mea', 300))
+    // cars.push(_createCar('Fiak Ibasa', 120))
+    // cars.push(_createCar('Subali Pesha', 100))
+    // cars.push(_createCar('Mitsu Bashi', 150))
+
+    utilService.saveToStorage(EMAIL_KEY, emails)
+  }
+  return emails
 }
 
 // function _createCar(vendor, maxSpeed = 250) {
 //     const car = getEmptyCar(vendor, maxSpeed)
-//     car.id = utilService.makeId() 
+//     car.id = utilService.makeId()
 //     return car
 // }
