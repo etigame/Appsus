ede5d8b6b7d51d4c9d83f2c4205cb9f10a74574e
Eti Gal-Or <etim139@gmail.com>
2022-11-09 11:32:42 +0200

    add more structure

--- routes.js
+++ routes.js
@@ -1,5 +1,10 @@
 import homePage from './views/app-home.cmp.js'
 import aboutPage from './views/app-about.cmp.js'
+import noteIndex from './apps/keep/pages/note-index.cmp.js'
+import noteDetails from './apps/keep/pages/note-details.cmp.js'
+import mailIndex from './apps/mail/pages/mail-index.cmp.js'
+import mailDetails from './apps/mail/pages/mail-details.cmp.js'
+
 
 const { createRouter, createWebHashHistory } = VueRouter
 
@@ -14,6 +19,22 @@ const routerOptions = {
 			path: '/about',
 			component: aboutPage,
 		},
+		{
+			path: '/keep',
+			component: noteIndex,
+		},
+		{
+            path: '/keep/:id',
+            component: noteDetails
+        },
+		{
+			path: '/mail',
+			component: mailIndex,
+		},
+		{
+            path: '/mail/:id',
+            component: mailDetails
+        },
 	],
 }
 
